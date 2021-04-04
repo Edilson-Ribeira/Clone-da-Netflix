73a01e2d1b5d35cfc74d413060b563eacd114b9f
Edilson Rodrigues dos Santos <erds-tecnologo@outlook.com.br>
2021-04-04 16:36:04 -0300

    Projeto inicial clone da Netflix

---
+++ js/botoes.js
@@ -0,0 +1,7 @@
+function assistir(){
+    window.open("https://www.youtube.com/watch?v=kpWMwSSl2PM");
+}
+
+function informacao(){
+    window.open("https://house-of-cards.fandom.com/pt-br/wiki/House_of_Cards");
+}
